472a2ad12546e7bceb27c72a67210a70c64d5d74
Christian Swan <dazzel@online.de>
2015-09-26 00:27:35 +0200

    check for webm or image files
    .webm files currently send as documents since Telegram only supports mp4 as video files.

--- pr0gramm/bot.py
+++ pr0gramm/bot.py
@@ -13,7 +13,7 @@ class Pr0grammBot:
                                  tmp_dir=config.get('b0t', 'tmp_dir'))
         self.__bot = telegram.Bot(token=config.get('b0t', 'token'))
         self.__tmp_dir = config.get('b0t', 'tmp_dir')
-        self.__tmp_image = ''
+        self.__tmp_image = {'ext': '', 'path': ''}
 
         # available commands with their corresponding bot method
         # scope the with __ so they are mangled by python
@@ -32,11 +32,11 @@ class Pr0grammBot:
     def __download_tmp_image(self, url):
         r = requests.get(url, stream=True)
 
-        ext = os.path.splitext(urlparse(url).path)[1]
+        self.__tmp_image['ext'] = os.path.splitext(urlparse(url).path)[1]
 
         if r.status_code == 200:
-            self.__tmp_image = os.path.join(self.__tmp_dir, str(int(time.time())) + ext)
-            with open(self.__tmp_image, 'wb') as f:
+            self.__tmp_image['path'] = os.path.join(self.__tmp_dir, str(int(time.time())) + self.__tmp_image['ext'])
+            with open(self.__tmp_image['path'], 'wb') as f:
                 for chunk in r:
                     f.write(chunk)
 
@@ -57,20 +57,36 @@ class Pr0grammBot:
                 getattr(self, '_' + self.__class__.__name__ + self.available_commands[text])(chat_id)
 
                 # clean up send image
-                os.remove(self.__tmp_image)
-                self.__tmp_image = ''
+                os.remove(self.__tmp_image['path'])
+                self.__tmp_image = {'ext': '', 'path': ''}
             except AttributeError:
                 print("could not call method", self.available_commands[text])
+
+    def __send_image(self, chat_id, data):
+        if self.__tmp_image['path'] is not '':
+            with open(self.__tmp_image['path'], 'rb') as f:
+                # TODO add caption -> tags, up and downvotes
+                try:
+                    if self.__tmp_image['ext'] == '.webm':
+                        # TODO convert webm to mp4
+                        self.__bot.sendDocument(chat_id=chat_id, document=f)
+                    else:
+                        self.__bot.sendPhoto(chat_id=chat_id, photo=f)
+
+                    return
+                except telegram.TelegramError as e:
+                    print(e)
+
+        self.__bot.sendMessage('Whoops. ¯\_(ツ)_/¯')
 
     def __send_top_sfw_image(self, chat_id):
         self.__bot.sendChatAction(chat_id=chat_id, action='upload_photo')
         data = self.__api.get_top_sfw_image()
 
+        # TODO save file id so we do not need to re-download and re-send an image
         self.__download_tmp_image(data['image'])
-        if self.__tmp_image is not '':
-            with open(self.__tmp_image, 'rb') as img:
-                # TODO add caption -> tags, up and downvotes
-                self.__bot.sendPhoto(chat_id=chat_id, photo=img)
+
+        self.__send_image(chat_id=chat_id, data=data)
 
     def run(self):
         for update in self.__bot.getUpdates(offset=self.__LAST_UPDATE_ID, timeout=10):

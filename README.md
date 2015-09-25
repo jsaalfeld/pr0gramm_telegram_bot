7fdfd3cf55b2f1df90e81af5585b54cc8368006c
Jochen Saalfeld <jsaalfeld@uni-osnabrueck.de>
2015-09-25 13:25:47 +0200

    chaning to readable config

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Pr0gramm Bot
 
 You need to pass your token from telegram as command line parameter and install via pip 'telegram-bot'
+And you need http://pyyaml.org/wiki/PyYAML
--- pr0gramm_bot.py
+++ pr0gramm_bot.py
@@ -3,22 +3,30 @@
 import telegram
 import sys
 import pr0gramm
+import ConfigParser
+
 
 def main(argv):
-	print str(argv)
-	if len(argv) == 3:
-		telegram_token = argv[0]
-		pr0gramm_username = argv[1]
-		pr0gramm_password = argv[2]
-		bot = telegram.Bot(token=telegram_token)
-		print bot.getMe()
-	else:
-		print_help()
+	config = ConfigParser.RawConfigParser(allow_no_value=True)
+	config.read('config.cfg')
+	telegram_token = config.get('b0t', 'token')
+	pr0gramm_username = config.get('b0t', 'username')
+	pr0gramm_password = config.get('b0t', 'password')
+	bot = telegram.Bot(token=telegram_token)
+	print "I'm the " + bot.getMe().username
+	updates = bot.getUpdates()
+	for update in updates:
+		print update.message.chat.id
+		bot.sendMessage(chat_id=update.message.chat.id, text="lol")
+	print [u.message.text for u in updates]
+
+
 def print_help():
 	print 'To start this bot you must pass 3 parameters'
 	print 'your telegram token'
 	print 'your pr0gramm username'
 	print 'your pr0gramm password'
+
 
 if __name__ == '__main__':
 	main(sys.argv[1:])

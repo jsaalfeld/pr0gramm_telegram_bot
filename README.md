21a7fb082e1720d632cef34a74d5f86496a51530
Jochen Saalfeld <jsaalfeld@uni-osnabrueck.de>
2015-09-25 14:26:45 +0200

    fix: reply to all messages in queue

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Pr0gramm Bot
 
 You need to pass your token from telegram as command line parameter and install via pip 'telegram-bot'
-And you need http://pyyaml.org/wiki/PyYAML
--- pr0gramm_bot.py
+++ pr0gramm_bot.py
@@ -4,29 +4,47 @@ import telegram
 import sys
 import pr0gramm
 import ConfigParser
+import datetime
+import time
 
 
 def main(argv):
 	config = ConfigParser.RawConfigParser(allow_no_value=True)
-	config.read('config.cfg')
+	configFileName = 'config.cfg'
+	config.read(configFileName)
+	configFile = open(configFileName,'w')
 	telegram_token = config.get('b0t', 'token')
 	pr0gramm_username = config.get('b0t', 'username')
 	pr0gramm_password = config.get('b0t', 'password')
 	bot = telegram.Bot(token=telegram_token)
+	last_update =  datetime.datetime.fromtimestamp(int(config.get('b0t', 'lastupdate')))
 	print "I'm the " + bot.getMe().username
 	updates = bot.getUpdates()
 	for update in updates:
-		print update.message.chat.id
-		bot.sendMessage(chat_id=update.message.chat.id, text="lol")
-	print [u.message.text for u in updates]
+		if update.message.date > last_update:
+			print "This Message is new"
+			if update.message.text.startswith("/"):
+				if update.message.text == "/sfw_beliebt":
+					sendTextMessage(bot,update.message.chat.id, "Hier kommt bald ihr foto")
+					sendFirstImageFromSFWPopular(bot,update.message.chat.id)
+				sendTextMessage(bot,update.message.chat.id,"Valid Command")
+			else:
+				sendTextMessage(bot,update.message.chat.id,telegram.Emoji.PILE_OF_POO)
+			sendTextMessage(bot,update.message.chat.id,"")
+		else:
+			print "This Message is old"
+	thisUpdate = updates[len(updates)-1].message.date
+	config.set('b0t', 'lastupdate', int(time.mktime(thisUpdate.timetuple())))
+	config.write(configFile)
 
+def sendTextMessage(bot, chat_id, text):
+	try:
+		bot.sendMessage(chat_id=chat_id, text=text)
+	except telegram.error.TelegramError, e:
+		print e.message
 
-def print_help():
-	print 'To start this bot you must pass 3 parameters'
-	print 'your telegram token'
-	print 'your pr0gramm username'
-	print 'your pr0gramm password'
-
+def sendFirstImageFromSFWPopular(bot, chat_id):
+	print "TODO"
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
